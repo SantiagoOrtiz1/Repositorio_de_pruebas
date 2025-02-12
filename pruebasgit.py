9c8a931aa6821e0f045fda9ed807ebc3ee1dcd29
Santiago Ortiz <miguelsanticas@gmail.com>
2025-02-12 13:11:33 -0500

    Este es el segundo cambio que hago a este código

--- pruebasgit.py
+++ pruebasgit.py
@@ -1,1 +1,9 @@
 print("Esta es mi primera prueba para aprender sobre Git")
+
+
+num1 = int(input("Ingresa el primer número "))
+num2 = int(input("Ingresa el segundo número "))
+
+suma = num1 + num2
+
+print("El resultado de la suma de los números que ingresaste es: ", suma)
